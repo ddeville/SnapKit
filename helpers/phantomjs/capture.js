dd5a0e390edbb4dbc9b2436c371c5a03d637be29
Damien DeVille <damien@realmacsoftware.com>
2013-04-07 18:44:03 +0100

    wibble

--- helpers/phantomjs/capture.js
+++ helpers/phantomjs/capture.js
@@ -22,9 +22,21 @@ viewportWidth = viewportWidth || 1280;
 userAgent = userAgent || 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.57 Safari/537.17';
 
 var page = require('webpage').create();
-
 page.viewportSize = {width: viewportWidth, height: 1024};
 page.settings.userAgent = userAgent;
+
+page.onError = phantom.onError = function() {
+	system.stderr.write("There was an unknown error while capturing the page");
+	phantom.exit(1);
+};
+
+page.open(url, function (status) {
+	if (status !== 'success') {
+		phantom.exit(1);
+	}
+	
+	window.setTimeout(snap, 60);
+});
 
 function snap() {
 	page.evaluate(function() {
@@ -45,16 +57,3 @@ function snap() {
 	system.stdout.write(JSON.stringify(result));
 	phantom.exit(0);
 }
-
-page.onError = phantom.onError = function() {
-	system.stderr.write("There was an unknown error while capturing the page");
-	phantom.exit(1);
-};
-
-page.open(url, function (status) {
-	if (status !== 'success') {
-		phantom.exit(1);
-	}
-	
-	window.setTimeout(snap, 60);
-});
