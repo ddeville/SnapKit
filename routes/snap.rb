31c706c61984318ccd1403cec75d526bd019a021
Damien DeVille <damien@realmacsoftware.com>
2013-04-08 22:18:03 +0100

    Better identification of the accept type. Use the new perform method on the phantomjs job

--- routes/snap.rb
+++ routes/snap.rb
@@ -8,26 +8,14 @@ helpers do
     {:error => error}.to_json;
   end
   
-  def snap(params)
-    url = params[:url];
-    halt 400, json_error('The URL is a required parameter') unless url;
-
-    begin
-      url = URI.parse url
-    rescue URI::InvalidURIError
-      halt 400, json_error('The URL is malformed');
+  def render_response(contentType)
+    if contentType == 'text/html'
+      return haml(:snap);
     end
-
-    halt 400, json_error('The URL is not valid') unless url.kind_of? URI::HTTP;
-
-    @phantomjs.url = url.to_s;
-    @phantomjs.viewportWidth = params[:viewport_width];
-    @phantomjs.userAgent = params[:user_agent];
-
-    websnap = @phantomjs.websnap();
-    halt 500, json_error("The websnap could not be processed (#{@phantomjs.snappingError})") unless (websnap && websnap.image);
     
-    websnap
+    if contentType == 'application/json'
+      return @websnap.to_json;
+    end
   end
 end
 
@@ -36,17 +24,36 @@ before '/snap' do
 end
 
 get '/snap' do
-  @websnap = snap params
+  url = params[:url];
+  halt 400, json_error('The URL is a required parameter') unless url;
   
-  if request.accept? 'text/html'
-    content_type 'text/html'
-    return haml(:snap);
+  begin
+    url = URI.parse url
+  rescue URI::InvalidURIError
+    halt 400, json_error('The URL is malformed');
   end
   
-  if request.accept? 'application/json'
-    content_type 'application/json'
-    return @websnap.to_json;
-  end
+  halt 400, json_error('The URL is not valid') unless url.kind_of? URI::HTTP;
   
-  halt 406;
+  @phantomjs.url = url.to_s;
+  @phantomjs.viewportWidth = params[:viewport_width];
+  @phantomjs.userAgent = params[:user_agent];
+  
+  @phantomjs.perform();
+  
+  @websnap = @phantomjs.websnap;
+  
+  halt 500, json_error("The websnap could not be processed (#{@phantomjs.snappingError})") unless (@websnap && @websnap.image);
+  
+  supportedContentTypes = ['application/json', 'text/html'];
+  
+  request.accept.each { |acceptType|
+    contentType = acceptType.to_str;
+    next unless supportedContentTypes.include? contentType;
+    
+    content_type contentType;
+    halt 200, render_response(contentType);
+  };
+  
+  render_response('application/json');
 end
